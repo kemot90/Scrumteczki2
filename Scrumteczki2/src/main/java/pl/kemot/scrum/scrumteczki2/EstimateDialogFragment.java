1c8a39419ee864fd7565c70cf587e8c1180a7d90
Tomasz Musiałowski <tomasz.musialowski@coi.gov.pl>
2013-10-17 15:14:10 +0200

    Daremna próba powiązania widoku z danymi z Dialogu.

--- Scrumteczki2/src/main/java/pl/kemot/scrum/scrumteczki2/EstimateDialogFragment.java
+++ Scrumteczki2/src/main/java/pl/kemot/scrum/scrumteczki2/EstimateDialogFragment.java
@@ -4,10 +4,13 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.NumberPicker;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import java.lang.reflect.Array;
 
@@ -36,12 +39,26 @@ public class EstimateDialogFragment extends DialogFragment {
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(context);
         View estimatePickerLayout = inflater.inflate(R.layout.estimate_picker, null);
-        NumberPicker picker = (NumberPicker) estimatePickerLayout.findViewById(R.id.estimateNumberPicker);
+        final View main = inflater.inflate(R.layout.main, null);
+        final NumberPicker picker = (NumberPicker) estimatePickerLayout.findViewById(R.id.estimateNumberPicker);
         picker.setMinValue(0);
         picker.setMaxValue(estimates.length - 1);
         picker.setDisplayedValues(labels);
         return dialogBuilder
                 .setView(estimatePickerLayout)
+                .setPositiveButton("Wybierz", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        TextView optimistic = (TextView) main.findViewById(R.id.optimistic);
+                        TextView pessimistic = (TextView) main.findViewById(R.id.pessimistic);
+                        Spinner leftEstimate = (Spinner) main.findViewById(R.id.estimateLeft);
+                        Spinner rightEstimate = (Spinner) main.findViewById(R.id.estimateRight);
+
+                        optimistic.setText(estimates[2].getLabel());
+                        rightEstimate.setSelection(2);
+                        main.refreshDrawableState();
+                    }
+                })
                 .create();
 
     }
